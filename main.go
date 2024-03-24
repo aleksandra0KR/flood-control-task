59d06fc629d4b001773a8a47b041d3caebf9894f
aleksandra2022itmo <kriuchkova.alexa@yandex.ru>
2024-03-24 16:43:44 +0300

    feat: add entry point

--- main.go
+++ main.go
@@ -1,11 +1,67 @@
 package main
 
 import (
+	"FloodControl/configs"
+	"FloodControl/internal/model"
+	"FloodControl/internal/repository"
+	"FloodControl/internal/usecase"
 	"context"
+	"fmt"
+	"github.com/redis/go-redis/v9"
+	"log"
+	"time"
 )
 
 func main() {
 
+	config, err := configs.InitConfig("config.yml")
+	if err != nil {
+		log.Fatalf("initialization of configs have failed %s", err.Error())
+	}
+
+	client := redis.NewClient(&redis.Options{
+		Addr:     config.Redis.Host + ":" + config.Redis.Port,
+		Password: "",
+		DB:       0,
+		Protocol: 3,
+	})
+
+	foodControlParam := model.FloodControlParameters{Interval: config.Interval, Limit: config.Limit}
+	repo := repository.NewRepository(client)
+	var floodControlImplementation FloodControl = usecase.NewQuestUseCaseImplementation(repo, foodControlParam)
+	ctx := context.Background()
+
+	for i := 0; i < 3; i++ {
+
+		result, err := floodControlImplementation.Check(ctx, config.UserID)
+		if err != nil {
+			fmt.Println("failed to check", err)
+		}
+
+		if result {
+			fmt.Println("successful pass")
+		} else {
+			fmt.Println("doesn't pass")
+		}
+
+		time.Sleep(time.Second)
+	}
+
+	for i := 0; i < 3; i++ {
+
+		result, err := floodControlImplementation.Check(ctx, config.UserID)
+		if err != nil {
+			fmt.Println("failed to check", err)
+		}
+
+		if result {
+			fmt.Println("successful pass")
+		} else {
+			fmt.Println("doesn't pass")
+		}
+
+		time.Sleep(time.Second * 6)
+	}
 }
 
 // FloodControl интерфейс, который нужно реализовать.
